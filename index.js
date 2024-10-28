779865081ceaad882f0db73dbb7cb6bb7141397a
Appru <j.appru@gmail.com>
2024-10-28 13:34:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json())
+
+app.listen(
+    PORT,
+()=> console.log("its working")
+)
+
+app.get('/frameid',(req,res)=>{
+    res.status(200).send({
+        123456: "img1",
+        56789: "img2",
+        10001: "img3"
+    })
+})
