13dd0f619ba14f013807fadb058816704c7972ab
Appru <j.appru@gmail.com>
2024-10-28 13:54:52 +0000

    add brackets

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.listen(
 )
 
 app.get('/frameid',(req,res)=>{
-    res.status(200).send({
+    res.status(200).send([{
         123456: "img1",
         56789: "img2",
         10001: "img3"
-    })
+    }])
 })
